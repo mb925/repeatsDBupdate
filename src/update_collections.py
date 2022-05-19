ee62f68522853b278e5ede571d797ae45d4f6564
martina <martina.bevilacqua5@libero.it>
2022-05-19 16:45:29 +0200

    scripts to update collections

--- src/update_collections.py
+++ src/update_collections.py
@@ -7,20 +7,15 @@ import shutil
 import pandas as pd
 import xml.etree.ElementTree as ET
 import config as cfg
-
-
-## add ORCID and date info in the json files, write entries collection and a list of the new pdbs
 from src.pdb_mapping import process_pdb
 
-rootdir = cfg.data['data']
-coll = cfg.data['collections']
-
+## add ORCID and date info in the json files, write entries collection and a list of the new pdbs
 def add_orchid():
 
     pdbs = {}
     i = 0
-    with open(coll + 'output_collections/new_entries.mjson', 'w') as fp:
-        for subdir, dirs, files in os.walk(rootdir):
+    with open(cfg.data['collections'] + '/output_collections/new_entries.mjson', 'w') as fp:
+        for subdir, dirs, files in os.walk(cfg.data['data']):
             for file in files:
                 print(os.path.join(subdir, file))
                 with open(os.path.join(subdir, file), 'rt') as f:
@@ -35,16 +30,16 @@ def add_orchid():
                       fp.write(json.dumps(line[0]) + '\n')
 
     df = pd.DataFrame.from_dict(pdbs, "index")
-    df.to_csv(coll + '/output_collections/new_pdbs_list.tsv', sep='\t', index=False, header=False)
+    df.to_csv(cfg.data['collections'] + '/output_collections/new_pdbs_list.tsv', sep='\t', index=False, header=False)
 
 
 ## create a new Entries collection copy ---but-- if the pdb is in new pdbs list, discard it (it will be overwritten by the new annotated ones)
 def create_old_entries_copy():
-    new_entries = pd.read_csv(rootdir + '/output_collections/new_pdbs_list.tsv', sep='\t', header=None)[0].to_list()
+    new_entries = pd.read_csv(cfg.data['collections'] + '/output_collections/new_pdbs_list.tsv', sep='\t', header=None)[0].to_list()
     print(new_entries)
     entries_dictionary = dict.fromkeys(new_entries, "")
-    with open(rootdir + '/output_collections/old_entries.mjson', 'w') as fp:
-        with open(rootdir + "/input_collections/entries_20201015.mjson", 'rt') as f:
+    with open(cfg.data['collections'] + '/output_collections/old_entries.mjson', 'w') as fp:
+        with open(cfg.data['collections'] + "/input_collections/entries_20201015.mjson", 'rt') as f:
             for line in f:
                 line = json.loads(line)
                 line_text = json.dumps(line)
@@ -54,54 +49,68 @@ def create_old_entries_copy():
 
 # concat the remaining pdbs to Entries collection
 def create_entries_collection():
-    shutil.copyfile(rootdir + '/output_collections/old_entries.mjson',
-                    rootdir + '/output_collections/entries_20220516.mjson')
+    shutil.copyfile(cfg.data['collections'] + '/output_collections/old_entries.mjson',
+                    cfg.data['collections'] + '/output_collections/entries_20220516.mjson')
 
-    with open(rootdir + '/output_collections/entries_20220516.mjson', 'a') as fp:
-        with open(rootdir + '/output_collections/new_entries.mjson', 'rt') as f:
+    with open(cfg.data['collections'] + '/output_collections/entries_20220516.mjson', 'a') as fp:
+        with open(cfg.data['collections'] + '/output_collections/new_entries.mjson', 'rt') as f:
             for line in f:
                 fp.write(line)
+
+def mapping(): # generate all xmls
+    ## transform PDB indexes of the new collection
+    i = 0
+    new_entries = pd.read_csv(cfg.data['collections'] + '/output_collections/new_pdbs_list.tsv', sep='\t', header=None)[0].to_list()
+    print(new_entries)
+    for pdb in new_entries:
+        if os.path.isfile(cfg.data['collections'] + '/output_collections/mappings/' + pdb + '.xml.gz'):
+            print("File exists")
+        else:
+            print("File does not exist, downloading")
+            process_pdb(pdb)
+            print(pdb)
+
+        i += 1
+        print(i)
+
+        df = pd.DataFrame(new_entries)
+        # keeping track of where we are if the process breaks
+        df.to_csv(cfg.data['collections'] + '/output_collections/to_map_pdbs_list.tsv', sep='\t', index=False, header=False)
+
 
 
 ## create a new Seqres collection copy ---but-- if the pdb is in new pdbs list, discard it (it will be overwritten by the new annotated ones)
 def create_old_seqres_copy():
-    new_entries = pd.read_csv(rootdir + '/output_collections/new_pdbs_list.tsv', sep='\t', header=None)[0].to_list()
+    new_entries = pd.read_csv(cfg.data['collections'] + '/output_collections/new_pdbs_list.tsv', sep='\t', header=None)[0].to_list()
     entries_dictionary = dict.fromkeys(new_entries, "")
 
-    with open(rootdir + '/output_collections/old_seqres.mjson', 'w') as fp:
-        with open(rootdir + "/input_collections/seqres_20201016.mjson", 'rt') as f:
+    with open(cfg.data['collections'] + '/output_collections/old_seqres.mjson', 'w') as fp:
+        with open(cfg.data['collections'] + "/input_collections/seqres_20201016.mjson", 'rt') as f:
             for line in f:
                 line = json.loads(line)
                 line_text = json.dumps(line)
                 if line['pdb_id'] not in entries_dictionary:
                     fp.write(line_text + '\n')
-# todo check why the last id is incomplete in output file
 
 
-def mapping(): # todo: finish generating all xmls and see if I can make the process faster (I don't need tsv for example)
-    ## transform PDB indexes of the new collection
-    new_entries = pd.read_csv(rootdir + '/output_collections/to_map_pdbs_list3.tsv', sep='\t', header=None)[0].to_list()
-    for pdb in new_entries:
-        process_pdb(pdb, rootdir) # todo: execute if you do not have the file already
-        new_entries.remove(pdb)
-        print(len(new_entries))
-        df = pd.DataFrame(new_entries)
-        df.to_csv(rootdir + '/output_collections/to_map_pdbs_list.tsv', sep='\t', index=False, header=False) # keep track of where we are because the process breaks (idk why, connection timeout)
+def create_new_seqres_collection(): # todo: not found EC ID e dssp
 
+    with open(cfg.data['collections'] + '/output_collections/new_seqres.mjson', 'w') as fp:
 
-def create_new_seqres_collection(): # not found EC ID e dssp
-
-    with open(rootdir + '/output_collections/new_seqres.mjson', 'w') as fp:
-
-        for file in os.listdir(rootdir + 'output_collections/mappings/'):
+        for file in os.listdir(cfg.data['collections'] + '/output_collections/mappings/'):
+            print(file)
             if file.endswith(".gz"):
 
-                inp = gzip.open(rootdir + 'output_collections/mappings/' + file, 'r')
+                inp = gzip.open(cfg.data['collections'] + '/output_collections/mappings/' + file, 'r')
 
                 tree = ET.parse(inp)
                 root = tree.getroot()
 
                 for x in root[2]:
+                    # I could take only pdb chain start end needed
+                    # but anyway seqres collection was already freaking big to begin with, it won't hurt if I store even more residues
+                    # the bright side is that next time these pdbs will be already downloaded and parsed
+
 
                     pdb_id = x.attrib['segId'].split('_')[0]
                     pdb_chain = x.attrib['segId'].split('_')[0]
@@ -141,9 +150,10 @@ def create_new_seqres_collection(): # not found EC ID e dssp
 
 # concat the old and new SeqRes collection
 def create_seqres_collection():
-    shutil.copyfile(rootdir + '/output_collections/old_seqres.mjson', rootdir + '/output_collections/seqres_20220516.mjson')
-    with open(rootdir + '/output_collections/seqres_20220516.mjson', 'a') as fp:
-        with open(rootdir + '/output_collections/new_seqres.mjson', 'rt') as f:
+    shutil.copyfile(cfg.data['collections'] + '/output_collections/old_seqres.mjson', cfg.data['collections'] + '/output_collections/seqres_20220516.mjson')
+    with open(cfg.data['collections'] + '/output_collections/seqres_20220516.mjson', 'a') as fp:
+        with open(cfg.data['collections'] + '/output_collections/new_seqres.mjson', 'rt') as f:
           for line in f:
             fp.write(line)
+# todo: not really sure why this collection is smaller than the previous one...
 
